8434afd23d24175cebfa43884ba3785d06dae619
hcamdclk8 <hcamdclk8@gmail.com>
2016-01-29 12:24:07 -0800

    v2 after watching video

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,14 +1,5 @@
-var forEach = function (points) {
-    
-    var revealPoint = function(index) {
-     points[index].style.opacity = 1;
-     points[index].style.transform = "scaleX(1) translateY(0)";
-     points[index].style.msTransform = "scaleX(1) translateY(0)";
-     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    for (i=0; i < points.length; i++) {
-        revealPoint(i);
+var forEach = function (array, callback) {
+    for (i=0; i < array.length; i++) {
+        callback(array[i]);
     }
-    
 }
