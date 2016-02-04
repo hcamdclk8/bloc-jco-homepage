f5cb3f0fc627545b54ac27e62a7df893727e898a
hcamdclk8 <hcamdclk8@gmail.com>
2016-02-04 13:30:32 -0800

    checkpoint 30 exercise refactor album and collection views to jquery

--- scripts/album.js
+++ scripts/album.js
@@ -58,31 +58,32 @@
       + '</tr>'
       ;
  
-     return template;
+     return $(template);
  };
 
 
 
 var setCurrentAlbum = function(album) {
 
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+     var $albumTitle = $('album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
  
      // Assign values to each part of album ie text, images
-     albumTitle.firstChild.nodeValue = album.name;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
+     $albumTitle.text(album.name);
+     $albumArtist.text(album.artist);
+     $albumReleaseInfo.text(album.year + ' ' + album.label);
+     $albumImage.attr('src', album.albumArtUrl);
  
      // clear album song list container
-     albumSongList.innerHTML = '';
+     $albumSongList.empty();
  
      // build list of songs from album.js 
      for (i = 0; i < album.songs.length; i++) {
-         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+         var $newRow = createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
+         $albumSongList.append($newRow);
      }
  };
 
